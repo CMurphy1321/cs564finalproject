3987a8bf42e518391d0fadd6103522a41ee6a760
Colin Murphy <cmurphy1321@gmail.com>
2016-09-15 23:31:19 +0000

    Change to postgresql.

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'cs564finalproject',
+	'USER': 'vagrant',
+	'PASSWORD': 'vagrant123',
+	'HOST': 'localhost',
+	'PORT': ''
     }
 }
 
